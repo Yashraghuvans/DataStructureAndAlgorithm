6146a021e7c7626f939a0903ab4c8ac1fa2d109a
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-06-16 23:15:17 +0530

    Create Patching_array.cpp

---
+++ Patching_array.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        
+    }
+};
+int main(){
+  int n;
+  cout<<"The final limit form 1 to ";
+  cin>>n;
+
+  vector<int>nums;
+
+  for(int i=0;i<n;i++){
+    cin>>nums[i];
+    }
+
+  Solution s;
+  s.minPatches(nums,n);
+
+  return 0;
+}
