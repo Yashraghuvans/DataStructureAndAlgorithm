d8008f119887ad560ec7afa06c67c41cf2dd1745
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-06-18 15:14:35 +0530

    Create MOst_Profit_Assigning.cpp

---
+++ Leetcode/Medium Leetcode/MOst_Profit_Assigning.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+        
+    }
+};
+
+int main(){
+  
+}
