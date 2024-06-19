dac1263e7ffc884ee037a42458e169596851d2d8
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-06-19 22:37:40 +0530

    Update Minimum_Number_of_Days_to_Make_m_Bouquets.cpp

--- Leetcode/Medium Leetcode/Minimum_Number_of_Days_to_Make_m_Bouquets.cpp
+++ Leetcode/Medium Leetcode/Minimum_Number_of_Days_to_Make_m_Bouquets.cpp
@@ -2,6 +2,50 @@
 using namespace std;
 
 class Solution {
+private:
+    bool possible(vector<int>arr,int day,int m,int k){
+        int cnt=0;
+        int noOfB=0;
+        int size=arr.size();
+        for(int i=0;i<size;i++){
+            if(arr[i]<=day){
+                cnt++;
+            }
+            else{
+                noOfB+=(cnt/k);
+                cnt=0;
+            }
+        }
+        noOfB+=(cnt/k);
+        return noOfB>=m;
+    }
+public:
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        long long val=m*1ll *k *1ll;
+        int n=bloomDay.size();
+        if(val>n)return -1;
+        int mini=INT_MAX;
+        int maxi=INT_MAX;
+
+        for(int i=0;i<n;i++){
+            mini=min(mini,bloomDay[i]);
+            maxi=max(maxi,bloomDay[i]);
+        }
+        int low=mini;
+        int high=maxi;
+        
+
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(possible(bloomDay,mid,m,k)){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
 
 };
 
