187bdfdbbd5c357a821e3a402e3b2e6036874685
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-06-20 23:49:46 +0530

    Update Magnetic_Force_Between_Two_Balls.cpp

--- Leetcode/Medium Leetcode/Magnetic_Force_Between_Two_Balls.cpp
+++ Leetcode/Medium Leetcode/Magnetic_Force_Between_Two_Balls.cpp
@@ -2,12 +2,38 @@
 using namespace std;
 
 class Solution {
+private:
+    bool canWePlace(const vector<int>& arr, int dist, int balls)  {
+        int countBalls = 1;
+        int lastPlaced = arr[0];
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr[i] - lastPlaced >= dist) {
+                countBalls++;
+                lastPlaced = arr[i];
+            }
+            if (countBalls >= balls) {
+                return true;
+            }
+        }
+        return false;
+    }   
 public:
     int maxDistance(vector<int>& position, int m) {
-        
+        sort(position.begin(), position.end());
+        int high = (position.back() - position[0]) / (m - 1);
+        int low = 1;
+        int ans = 1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(canWePlace(position,mid,m)){
+                ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return ans;
+
     }
 };
-int main(){
-  
-  return 0;
-}
