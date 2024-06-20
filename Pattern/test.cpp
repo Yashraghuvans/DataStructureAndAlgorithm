37dc22f59217d7fe09fb3591954eb1f8814343c8
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-06-20 22:55:38 +0530

    Create test.cpp

---
+++ Pattern/test.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    int pattern1(int n){
+        for(int i=1; i<n; i++){
+            for (int j = 1; j <= i; j++)
+            {
+                cout<<" * ";
+            }
+            cout<<endl;
+        }
+            
+    }
+    int pattern2(int n){
+        for(int i=1; i<=n; i++){
+            for (int j = 1; j < i; j++)
+            {
+                cout<<"  ";
+            }
+            for(int j = i; j < n; j++){
+                cout<<" * ";
+            }
+            cout<<endl;
+        }
+    }
+    int pattern3(int n){
+        for(int i=1; i<=n; i++){
+            for (int j = n; j > i; j--)
+            {
+                cout<<"  ";
+            }
+            for(int j = 0; j <2*i-1; j++){
+                cout<<"* ";
+            }
+            cout<<endl;
+        }
+    }
+};
+
+
+int main(){
+    int n;
+    cin>>n;
+
+    Solution s;
+    //pattern 1
+    s.pattern1(n);
+    //pattern 2
+    s.pattern2(n);
+    //pattern 3
+    s.pattern3(n);
+    return 0;
+}
