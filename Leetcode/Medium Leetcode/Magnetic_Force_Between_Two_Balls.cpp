65245136b3502e0e2ccb247c61b82e4363c0da41
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-06-20 21:14:16 +0530

    Create Magnetic_Force_Between_Two_Balls.cpp

---
+++ Leetcode/Medium Leetcode/Magnetic_Force_Between_Two_Balls.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxDistance(vector<int>& position, int m) {
+        
+    }
+};
+int main(){
+  
+  return 0;
+}
