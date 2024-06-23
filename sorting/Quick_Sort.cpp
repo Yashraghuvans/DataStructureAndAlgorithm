2ed4ea5a9a9e820251ffe0262d302381075a032c
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-06-23 18:13:21 +0530

    Create Quick_Sort.cpp

---
+++ sorting/Quick_Sort.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    int sort(vector<int>& arr){
+        
+    }
+};
+
+int main(){
+    int n;
+    vector<int>arr;
+    cin>>n;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    Solution s;
+    s.sort(arr);
+    return 0;
+}
