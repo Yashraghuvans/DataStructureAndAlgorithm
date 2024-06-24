ef71d52457c4a54f255ad86335d9af9ad39b2c05
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-06-24 12:05:22 +0530

    Update Quick_Sort.cpp

--- sorting/Quick_Sort.cpp
+++ sorting/Quick_Sort.cpp
@@ -3,8 +3,29 @@ using namespace std;
 
 class Solution{
     public:
-    int sort(vector<int>& arr){
-        
+    int partition(vector<int>& arr,int low,int high){
+        int pivat=arr[low];
+        int i=high;
+        int j=low;
+        while(i<j){
+            while(arr[i]<=pivat && i<=high-1){i++;}
+            while(arr[j]>pivat && j>=low+1){j--;}
+            if(i<j){
+                swap(arr[i],arr[j]);
+            }
+        }swap(arr[low],arr[j]);
+        return j;
+    }
+    void qs(vector<int>&arr,int low,int high){
+        if(low<high){
+            int pIndex=partition(arr,low,high);
+            qs(arr,low,pIndex-1);
+            qs(arr,pIndex+1,high);
+        }
+    }
+    vector<int>quicksort(vector<int>arr){
+        qs(arr,0,arr.size()-1);
+        return arr;
     }
 };
 
