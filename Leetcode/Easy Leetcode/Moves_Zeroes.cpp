8736e798207969302acb083c9ef1c124b1bdaf62
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-07-04 18:57:45 +0530

    Update Moves_Zeroes.cpp

--- Leetcode/Easy Leetcode/Moves_Zeroes.cpp
+++ Leetcode/Easy Leetcode/Moves_Zeroes.cpp
@@ -2,11 +2,38 @@
 using namespace std;
 
 class Solution{
-  public:
-  int test(){
-    
-  }
+    void ans1(vector<int>& nums) {
+      //brute force approch
+        vector<int>temp;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                temp.push_back(nums[i]);
+            }
+        }
+        for(int i=0;i<temp.size();i++){
+            nums[i]=temp[i];
+        }
+        int n=temp.size();
+        for(int i=n;i<nums.size();i++){
+            nums[i]=0;
+        }
+
+    }
 };
 int main(){
+  int n;
+  cin>>n;
+  vector<int>nums;
+  for(int i=0;i<n;i++){
+    cin>>nums[i];
+  }
+  Solution s;
+  //brute force
+  s.ans1(nums);
+  //optimised approch
+  s.ans2(nums);
+  //optimal approch
+  s.ans3(nums);
+  
   return 0;
 }
