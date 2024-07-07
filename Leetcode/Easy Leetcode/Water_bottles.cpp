e2d23eb3e0e7f6bb8e4f8d1598541f227926d398
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-07-07 21:21:29 +0530

    Create Water_bottles.cpp

---
+++ Leetcode/Easy Leetcode/Water_bottles.cpp
@@ -0,0 +1,19 @@
+#inlcude <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+         int totalBottles = numBottles;
+
+        while (numBottles >= numExchange) {
+            totalBottles += numBottles / numExchange;
+            numBottles = (numBottles / numExchange) + (numBottles % numExchange);
+        }
+
+        return totalBottles;
+    }
+};
+int main(){
+  Solution s;
+}
