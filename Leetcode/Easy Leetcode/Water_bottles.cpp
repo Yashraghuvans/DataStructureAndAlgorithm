15a50886821b234853099a2b0eae8aa34411e564
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-07-08 19:11:51 +0530

    Update Water_bottles.cpp

--- Leetcode/Easy Leetcode/Water_bottles.cpp
+++ Leetcode/Easy Leetcode/Water_bottles.cpp
@@ -13,6 +13,16 @@ public:
 
         return totalBottles;
     }
+    int numWaterBottles1(int numBottles, int numExchange){
+        int ans=numBottles;
+         while (numBottles >= numExchange) {
+            int rem=numBottles%numExchange;
+            numBottles=numBottles/numExchange;
+            ans+=numBottles;
+            numBottles+=rem;
+        }
+        return ans;
+    }
 };
 int main(){
   Solution s;
@@ -20,5 +30,7 @@ int main(){
     cin>>numBottles;
     cin>>numExchange;
     int ans=s.numWaterBottles(numBottles,numExchange);
+    int ans1=s.numWaterBottles1(numBottles,numExchange);
     cout<<ans<<endl;
+    cout<<ans1<<endl;
 }
