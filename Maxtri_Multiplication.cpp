e86dc8b6f99b6cdcbc92d7375e4fa26ac2d40255
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-07-12 15:15:41 +0530

    Create Maxtri_Multiplication.cpp

---
+++ Maxtri_Multiplication.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+   vector<vector<int>>mat1(3,vector<int>(3));
+    cout<<"Enter the first matrix : "<<endl;
+     for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cin >> mat1[i][j];
+        }
+    }
+    vector<vector<int>>mat2(3,vector<int>(3));
+    cout<<"Enter the first matrix : "<<endl;
+     for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cin >> mat2[i][j];
+        }
+    }
+    vector<vector<int>>mat_mult(3,vector<int>(3));
+    mat_mult[0][0]=0;
+      
+  
+}
