730323d8f2a92dcd178446307729ff18a00d8801
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-07-12 15:19:18 +0530

    Update Maxtri_Multiplication.cpp

--- Maxtri_Multiplication.cpp
+++ Maxtri_Multiplication.cpp
@@ -10,7 +10,7 @@ int main(){
         }
     }
     vector<vector<int>>mat2(3,vector<int>(3));
-    cout<<"Enter the first matrix : "<<endl;
+    cout<<"Enter the second matrix : "<<endl;
      for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             cin >> mat2[i][j];
@@ -18,6 +18,18 @@ int main(){
     }
     vector<vector<int>>mat_mult(3,vector<int>(3));
     mat_mult[0][0]=0;
-      
-  
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            for(int k=0;k<3;k++){
+               mat_mult[i][j]+=mat1[k][i]*mat2[j][k];
+            }
+        }
+    }
+   cout<<"matrix : "<<endl;
+     for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cin >> mat_mutl[i][j];
+        }
+    }
+  return 0;
 }
