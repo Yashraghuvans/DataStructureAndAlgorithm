45268445527d2eea86dcd11b0948ccda9298378d
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-07-19 14:13:32 +0530

    Create Sorted_Array_Search.cpp

---
+++ Geeks_for_geeks/Sorted_Array_Search.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int N,K,arr[N];
+  cin>>N;
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+  cin>>K;
+  Solution s;
+  return 0;
+}
