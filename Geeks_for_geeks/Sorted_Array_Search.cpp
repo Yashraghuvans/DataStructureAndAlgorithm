60a6d0254bef51f6fec19e75d79186e880d62748
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-07-19 14:16:40 +0530

    Update Sorted_Array_Search.cpp

--- Geeks_for_geeks/Sorted_Array_Search.cpp
+++ Geeks_for_geeks/Sorted_Array_Search.cpp
@@ -1,6 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution{
+Public:
+int searchInSorted(int arr[], int N, int K) 
+    { 
+        int ans=-1;
+        int val=0;
+       for(int i=0;i<N;i++){
+           if(arr[i]==K){
+               val++;
+               break;
+           }
+          
+       }
+       return(val==0)?ans:val;
+    }
+};
+
 int main(){
   int N,K,arr[N];
   cin>>N;
@@ -9,5 +26,7 @@ int main(){
   }
   cin>>K;
   Solution s;
+  int ans=s.searchInSorted(arr[],N,K);
+  cout<<ans<<endl;
   return 0;
 }
