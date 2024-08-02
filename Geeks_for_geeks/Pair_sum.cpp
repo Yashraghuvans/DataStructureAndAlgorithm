cc13a35420fdd24a1731bcfdbd1a33404ab205d7
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-08-02 10:50:59 +0530

    Update Pair_sum.cpp

--- Geeks_for_geeks/Pair_sum.cpp
+++ Geeks_for_geeks/Pair_sum.cpp
@@ -1,5 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+class Solution{
+  vector<vector<int>>pairSum(vector<int>&arr,int s){
+    vector<vector<int>>ans;
+    for(int i=0;i<arr.size();i++){
+      for(int j=i+1;j<arr.size();j++){
+        if(arr[i]+arr[j]==s){
+          vector<int>temp;
+          temp.push_back(min(arr[i],arr[j]));
+          temp.push_back(max(arr[i],arr[j]));
+          ans.push_back(temp);
+        }
+      }
+    }
+    sort(ans.begin(),ans.end());
+    return ans;
+  }
+};
+
 int main(){
+  vector<int>arr;
+  int sum;
+  int size;
+
+  cin>>size;
+  cin>>sum;
+
+  for(int i=0;i<size;i++){
+    cin>>arr[i];
+  }
+  Solution s;
+  vector<vector<int>>pair;
+  pair.push_back(s.pairSum(arr,sum));
   return 0;
 }
