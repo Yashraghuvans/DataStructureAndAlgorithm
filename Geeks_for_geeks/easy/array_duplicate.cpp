ec6008e436fc3910210870e4992c42a846dec035
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-08-05 22:04:22 +0530

    Update array_duplicate.cpp

--- Geeks_for_geeks/easy/array_duplicate.cpp
+++ Geeks_for_geeks/easy/array_duplicate.cpp
@@ -1,6 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution{
+ public:
+    vector<int> duplicates(vector<long long> arr) {
+        map<int,int>mp;
+        for (int i=0;i<arr.size();i++){
+            mp[arr[i]]++;
+        }
+        vector<int>s;
+         for (auto p:mp){
+            if (p.second>1){
+                s.push_back(p.first);
+            }
+        }
+        if (s.size()==0 ){
+            return {-1};
+        }
+        else{
+            return s;
+        }
+    }
+};
 
 int main()
 {
