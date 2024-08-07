866ae10f22ac689fc4d216d34562038a60bae303
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-08-07 22:01:54 +0530

    Update rotate_array.cpp

--- Geeks_for_geeks/easy/rotate_array.cpp
+++ Geeks_for_geeks/easy/rotate_array.cpp
@@ -1,6 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+class Solution{
+  public:
+  vector<int> rotateArray(vector<int>arr, int k) {
+    int size=arr.size();
+    vector<int>ans(size);
+    for(int i=0;i<size;i++){
+        ans[i]=arr[(i+k)%size];
+    }
+    return ans;
+    
+  }
+};
 int main(){
   return 0;
 }
