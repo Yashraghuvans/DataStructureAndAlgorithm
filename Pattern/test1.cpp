a338f66a6be66379599a70238e5633d62b36b08d
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-08-09 22:06:09 +0530

    Update test1.cpp

--- Pattern/test1.cpp
+++ Pattern/test1.cpp
@@ -37,6 +37,21 @@ class Solution{
             cout<<endl;
         }
     }
+    int pattern4(int n){
+        for (int i = n; i > 0; i--) {
+            for (int j = 1; j <= n; j++) 
+            {
+                if (j >= i) {
+                    cout << "* ";
+                }
+                else {
+                    cout << "  ";
+                }
+            }
+            cout << endl;
+        }
+        
+    }
 };
 
 
