3d3d1e06b16159d2c4739458ed86e490f0ddd391
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-08-17 18:10:56 +0530

    Create Two_sum.cpp

---
+++ Leetcode/Medium Leetcode/Two_sum.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  vector<int>arr;
+  int n;
+  Solution s;
+
+  cin>>n;
+
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+
+  vector<int> ans;
+  
+  return 0;
+}
