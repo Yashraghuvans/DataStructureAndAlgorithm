40a09b465404f3aa5a988f7e9e1e86a4380fae64
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-08-18 16:22:59 +0530

    Update Sort_012.cpp

--- Leetcode/Medium Leetcode/Sort_012.cpp
+++ Leetcode/Medium Leetcode/Sort_012.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 class Solution{
+public:
   void sort012(int *arr,int n){
     for(int i=0;i<n;i++){
       for(int j=0;j<n;j++){
@@ -13,10 +14,30 @@ class Solution{
         }
      }
   }
-void 1sort012(int *arr, int n){
-   sort(arr+0,arr+n);
-   
-  }
+
+  void sortColors(vector<int>& nums) {
+        int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) cnt0++;
+            else if (nums[i] == 1) cnt1++;
+            else cnt2++;
+        }
+        int index = 0;
+        while (cnt0 > 0) {
+            nums[index++] = 0;
+            cnt0--;
+        }
+        while (cnt1 > 0) {
+            nums[index++] = 1;
+            cnt1--;
+        }
+        while (cnt2 > 0) {
+            nums[index++] = 2;
+            cnt2--;
+        }
+    }
+
+
 };
 
 int main(){
