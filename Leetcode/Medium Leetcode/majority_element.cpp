d9edf7a2bbf078598a9de434ccfa88764803e24a
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-08-23 21:36:07 +0530

    Create majority_element.cpp

---
+++ Leetcode/Medium Leetcode/majority_element.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution{
+public :
+    int ans(vector<int>&arr){
+        int cnt;
+        int fin;
+        int n=arr.size();
+        for(int i=0;i<arr.size();i++){
+            cnt=0;
+            for(int j=0;j<arr.size();j++){
+                if(arr[j]==arr[i])cnt++;
+            }
+            if(cnt>arr.size()/2)fin= arr[i];
+        }
+       return fin; 
+    }
+};
+
+
+
+
+int main() {
+    
+    vector<int> arr = {1, 2, 2, 3, 2};
+    Solution solution;
+
+   
+    int majorityElement = solution.ans(arr);
+
+ 
+    cout << "Majority element: " << majorityElement << endl;
+    return 0;
+}
