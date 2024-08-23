a09e8977bbc81475b415efd3098f541b0cdb1fdf
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-08-23 22:55:43 +0530

    Update majority_element.cpp

--- Leetcode/Medium Leetcode/majority_element.cpp
+++ Leetcode/Medium Leetcode/majority_element.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 class Solution{
 public :
+
+//brute force
 int ans(vector<int>&arr){
         int cnt;
         int fin;
@@ -17,7 +19,9 @@ int ans(vector<int>&arr){
             if(cnt>arr.size()/2)fin= arr[i];
         }
        return fin; 
-    }
+}
+
+//better
 int ans2(vector<int>&arr){
     map<int,int>mpp;
     int n=arr.size();
@@ -31,6 +35,36 @@ int ans2(vector<int>&arr){
     }
     return -1;
 }
+
+//optimal 
+//moor's voting algorithm
+int ans3(vector<int> v) {
+	
+	int cnt=0;
+	int element;
+	int n=v.size();
+	for(int i=0;i<n;i++){
+		if(cnt==0){
+			cnt=1;
+			element=v[i];
+		}
+		else if(v[i]==element){
+			cnt++;
+		}
+		else{
+			cnt--;
+		}
+	}
+	int ans;
+	for(int i=0;i<n;i++){
+		if(v[i]==element)ans++;
+	}
+	if(ans>(n/2)){
+		return element;
+	}
+	return -1;
+
+}
 };
 
 
@@ -42,9 +76,13 @@ int main() {
     Solution solution;
 
    
-    int majorityElement = solution.ans(arr);
+    int ans = solution.ans(arr);
+    cout << "Majority element: " << ans << endl;
 
- 
-    cout << "Majority element: " << majorityElement << endl;
+    int ans2 = solution.ans2(arr);
+    cout << "Majority element: " << ans2 << endl;
+        
+    int ans3 = solution.ans3(arr);
+    cout << "Majority element: " << ans3 << endl;
     return 0;
 }
