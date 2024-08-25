5af7229e180a5f1bef28be784fc2d82994f20059
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-08-25 23:38:33 +0530

    Update Alternate_Number.cpp

--- Leetcode/Medium Leetcode/Alternate_Number.cpp
+++ Leetcode/Medium Leetcode/Alternate_Number.cpp
@@ -20,4 +20,39 @@ vector<int> alternateNumbers(vector<int>&a) {
     return ans;
  
 }
+vector<int> alternateNumbersNew(vector<int>&a) {
+    vector<int>pos,neg;
+    int n=a.size();
+    for(int i=0;i<n;i++){
+        if(a[i]<0){
+            neg.push_back(a[i]);
+        }
+        else{
+            pos.push_back(a[i]);
+        }
+    }
+    if(pos.size()>neg.size()){
+        for(int i=0;i<neg.size();i++){
+            a[i*2]=pos[i];
+            a[i*2+1]=neg[i];
+        }
+        int index=2*neg.size();
+        for(int i=neg.size();i<pos.size();i++){
+            a[index]=pos[i];
+            index++;
+        }
+    }
+    else{
+        for(int i=0;i<pos.size();i++){
+            a[i*2]=pos[i];
+            a[i*2+1]=neg[i];
+        }
+        int index=2*pos.size();
+        for(int i=pos.size();i<neg.size();i++){
+            a[index]=neg[i];
+            index++;
+        }
+    }
+    return a;
+}
 };
