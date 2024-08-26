bf911cac09a5a5a11a8c63ef10abed938b3b2e0f
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-08-26 10:26:34 +0530

    Update buy_sell_stocks.cpp

--- Leetcode/Medium Leetcode/buy_sell_stocks.cpp
+++ Leetcode/Medium Leetcode/buy_sell_stocks.cpp
@@ -1,19 +1,19 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
-class Solution{
-int maximumProfit(vector<int> &arr){
-    int mini=arr[0];
-    int maxm=0;
-    
-    for(int i=0;i<arr.size();i++){
-      int cost=arr[i]-mini;
-      maxm=max(maxm,cost); 
-      mini=min(mini,arr[i]);
-       
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minimum=prices[0];
+        int maximum=0;
+
+        for(int i=0;i<prices.size();i++){
+            int profit=prices[i]-minimum;
+            maximum=max(maximum,profit);
+            minimum=min(minimum,prices[i]);
+        }
+        return maximum;
     }
-  return maxm;
-}
 };
 
 int main() {
