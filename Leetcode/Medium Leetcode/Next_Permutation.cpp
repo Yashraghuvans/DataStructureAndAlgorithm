4f27f2468ed570ae87a4bacec95dead7a4221557
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-08-28 20:55:20 +0530

    Update Next_Permutation.cpp

--- Leetcode/Medium Leetcode/Next_Permutation.cpp
+++ Leetcode/Medium Leetcode/Next_Permutation.cpp
@@ -6,6 +6,38 @@ public:
     void nextPermutation(vector<int>& nums) {
         next_permutation(nums.begin(),nums.end());
     }
+
+    void nextPermutation(vector<int>&nums){
+        int index=-1;
+        int n=nums.size();
+
+        //next prefix
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                index=i;
+                break;
+            }
+        }
+
+        if(index==-1){
+            reverse(nums.begin(),nums.end());
+        }
+
+        else{
+
+        //find just min element
+        for(int i=n-1;i>=index;i--){
+            if(nums[i]>nums[index]){
+                swap(nums[i],nums[index]);
+                break;
+            }
+        }
+
+        //revers left out array
+
+        reverse(nums.begin()+index+1,nums.end());
+        }
+    }
 };
 
 int main(){
