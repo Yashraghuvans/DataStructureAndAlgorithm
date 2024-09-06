2e88976b57a0add3cd4d402d73195890dfc0875f
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-09-06 22:29:50 +0530

    Update Patterns.cpp

--- Pattern/Patterns.cpp
+++ Pattern/Patterns.cpp
@@ -129,6 +129,20 @@ class Solution{
         cout<<endl;
         }
     }
+   void pattern9(int n){
+     for(int i=n;i>0;i--){
+        for(int j=2*i-1;j>0;j--){
+            cout<<"*";
+        }
+        for(int j=0;j<n-i;j++){
+            cout<<" ";
+        }
+        for(int j=2*i-1;j>0;j--){
+            cout<<"*";
+        }
+         cout<<endl;
+        }
+    }
 };
 
 
