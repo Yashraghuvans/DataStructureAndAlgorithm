4a3fc7c7898ea784a3287db6ba6ffc69d38393a8
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-09-11 21:43:44 +0530

    Create Intersection_of_Two_Arrays_II.cpp

---
+++ Leetcode/Array/Easy Leetcode/Intersection_of_Two_Arrays_II.cpp
@@ -0,0 +1,36 @@
+#inlcude<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        
+        int i = 0, j = 0;
+        vector<int> result;
+        
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                result.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        
+        return result;
+    }
+};
+
+int main(){
+    vector<int>nums1={1,2,2,1};
+    vector<int>={2,2};
+    Solution s;
+    s.intersect(nums1,nums2);
+    return 0;
+}
