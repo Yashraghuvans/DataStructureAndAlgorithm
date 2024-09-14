5386fc72e6a79a4263090ad6d96e7a6afe944a55
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-09-14 19:48:05 +0530

    Update position_element.cpp

--- Leetcode/Binary Search/Easy/position_element.cpp
+++ Leetcode/Binary Search/Easy/position_element.cpp
@@ -2,6 +2,45 @@
 
 using namespace std;
 
+class Solution {
+public:
+    int lb(vector<int>& nums, int n, int x) {
+        int low=0;int high=n-1;int ans=n;
+
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]>=x){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+    int ub(vector<int>& nums, int n, int x) {
+        int low=0;int high=n-1;int ans=n;
+
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]>x){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n=nums.size();
+        int low=lb(nums,n,target);
+        if(low==n || nums[low] != target)return {-1,-1};
+        return{low,ub(nums,n,target)-1};
+    }
+};
 int solve(int n, int key, vector < int > & v) {
   int start = 0;
   int end = n - 1;
