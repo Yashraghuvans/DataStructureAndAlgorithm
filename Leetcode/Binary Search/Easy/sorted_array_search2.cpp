0980cd6690f110787f74837cb9ac0968a0581158
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-09-15 14:13:10 +0530

    Create sorted_array_search2.cpp

---
+++ Leetcode/Binary Search/Easy/sorted_array_search2.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {}
+};
+int main() {
+    Solution s;
+    return 0;
+}                      
