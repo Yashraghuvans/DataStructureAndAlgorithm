cf1b2f3ce1263dea33a18d8c8a75c01e1f5d00b8
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-09-15 14:43:22 +0530

    Update sorted_array_search2.cpp

--- Leetcode/Binary Search/Easy/sorted_array_search2.cpp
+++ Leetcode/Binary Search/Easy/sorted_array_search2.cpp
@@ -4,7 +4,36 @@ using namespace std;
 
 class Solution {
 public:
-    bool search(vector<int>& nums, int target) {}
+    bool search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int low=0,high=n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]==target)return true;
+            else if(nums[low]==nums[mid] && nums[mid]==nums[high]){
+                low++;
+                high--;
+                continue;
+            }
+            else if(nums[low]<=nums[mid]){
+                if(target>=nums[low] && target <= nums[mid]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            else{
+                if(target<=nums[high]&& nums[mid]<=target){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
+            }
+        }
+        return false;
+    }
 };
 int main() {
     Solution s;
