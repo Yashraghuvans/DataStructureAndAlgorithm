92b8ab1d8a5f7a2dda7c110a8650591b65028073
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-09-15 22:54:42 +0530

    Create single_element_sortedArray.cpp

---
+++ Leetcode/Binary Search/Medium/single_element_sortedArray.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        map<int,int>mpp;
+        for(int i=0;i<nums.size();i++){
+            mpp[nums[i]]++;
+        }
+        for(auto it:mpp){
+            if(it.second==1){
+                return it.first;
+            }
+        }
+        return -1;
+    }
+};
+int main(){
+    Solution s;
+    return 0;
+}
