99d764fcfbbb821d7e68bcf1a5b9cfdcb991cea3
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-09-16 23:05:31 +0530

    Create peak_element.cpp

---
+++ Leetcode/Binary Search/Medium/peak_element.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(  ((i==0)||(nums[i-1]<nums[i]))  && ((i==n-1)||(nums[i]>nums[i+1]))){
+                return i;
+            }
+        }
+        return 0;
+    }
+};
+
+int main(){
+    Solution s;
+    return 0;
+}
