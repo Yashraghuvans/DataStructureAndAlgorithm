68333d3a607ee5c6b741b63e5d99fb8fd3d6ff70
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-09-16 23:16:46 +0530

    Update peak_element.cpp

--- Leetcode/Binary Search/Medium/peak_element.cpp
+++ Leetcode/Binary Search/Medium/peak_element.cpp
@@ -12,6 +12,26 @@ public:
         }
         return 0;
     }
+        int findPeakElement1(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1)return 0;
+        if(nums[0]>nums[1])return 0;
+        if(nums[n-1]>nums[n-2])return n-1;
+        
+
+        int low=0;int high=n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]>nums[mid-1]&&nums[mid]>nums[mid+1]){
+                return mid;
+            }
+            else if(nums[mid]>nums[mid-1]){
+                low=mid+1;
+            }
+            else high=mid-1;
+        }
+        return -1;
+    }
 };
 
 int main(){
