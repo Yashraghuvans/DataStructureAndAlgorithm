3639d39bb360792b2cc89f0bfd3982b6e7c6eedc
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-09-19 18:48:54 +0530

    Create Two_sum.cpp

---
+++ Leetcode/Array/Easy Leetcode/Two_sum.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(std::vector<int>& nums, int target) {
+        unordered_map<int, int> num_indices;
+        vector<int> result;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+            if (num_indices.find(complement) != num_indices.end()) {
+                result.push_back(num_indices[complement]);
+                result.push_back(i);
+                return result;
+            }
+            num_indices[nums[i]] = i;
+        }
+
+        return result;
+    }
+};
+
+int main(){
+  return 0;
+}
