04231087079381843ab82ccc18481506946b6e1f
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-09-25 18:41:04 +0530

    Create split_array_largest_sum.cpp

---
+++ Leetcode/Binary Search/Hard/split_array_largest_sum.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countStudents(vector<int>& arr, int pages) {
+        int n = arr.size(); 
+        int students = 1;
+        long long pagesStudent = 0;
+        for (int i = 0; i < n; i++) {
+            if (pagesStudent + arr[i] <= pages) {
+                
+                pagesStudent += arr[i];
+            } else {
+               
+                students++;
+                pagesStudent = arr[i];
+            }
+        }
+        return students;
+    }
+    int splitArray(vector<int>& arr, int m) {
+
+      
+        int n=arr.size();
+        int low = *max_element(arr.begin(), arr.end());
+        int high = accumulate(arr.begin(), arr.end(), 0);
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int students = countStudents(arr, mid);
+            if (students > m) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+};
