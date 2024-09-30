787cccc698e73e54b03e7de6a941695f085caf78
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-09-30 20:04:57 +0530

    Create Remove_Outmost_parenthese.cpp

---
+++ Leetcode/String/Easy/Remove_Outmost_parenthese.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string result;
+        int balance = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                if (balance > 0) {
+                    result += s[i];
+                }
+                balance++;
+            } else {
+                balance--;
+                if (balance > 0) {
+                    result += s[i];
+                }
+            }
+        }
+
+        return result;
+    }
+};
+int main(){
+  return 0;
+}
