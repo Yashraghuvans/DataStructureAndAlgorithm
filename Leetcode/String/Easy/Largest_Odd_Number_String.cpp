39021cf964f5525923e82e4767220d4e3a33556b
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-10-01 22:41:54 +0530

    Update Largest_Odd_Number_String.cpp

--- Leetcode/String/Easy/Largest_Odd_Number_String.cpp
+++ Leetcode/String/Easy/Largest_Odd_Number_String.cpp
@@ -20,6 +20,16 @@ public:
 
         return ans;
     }
+    string largestOddNumber(string num) {
+        int n=num.size();
+        string ans="";
+        int i;
+        for(i=n-1; i>=0; i--){
+            if((num[i]-'0')%2 != 0)
+            break;
+        }
+        return num.substr(0,i+1);
+    }
 };
 int main(){
   return 0;
