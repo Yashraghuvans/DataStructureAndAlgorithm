b457fddc92cd99101078ff1cfc533057c60974d2
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-10-05 11:09:03 +0530

    Create vector_work.cpp

---
+++ STL Library/vector_work.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+    int sum(vector<int>& v){
+        int sum=accumulate(v.begin(),v.end(),0);
+        return sum;
+    }
+    int highest(vector<int>& v){
+        return *max_element(v.begin(),v.end());
+    }
+    int permutation(vector<int>& v){
+        next_permutation(v.begin(),v.end());
+        return 0;
+    }
+
+};
+
+int main(){
+    Solution s;
+    int n;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    return 0;
+}
