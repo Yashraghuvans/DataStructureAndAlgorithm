cc7c4705022e5d734aa3bd7e7de28158f83224f7
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-10-05 21:36:27 +0530

    Create Adding_at_end.cpp

---
+++ Geeks_for_geeks/Linked List/Adding_at_end.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int data1,Node* next1){
+        data = data1;
+        next = next1;
+    }
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+
+class Solution {
+  public:
+    Node *insertAtEnd(Node *head, int x) {
+        if (head == NULL) {
+            Node *curr = new Node(x);
+            return curr;
+            }
+        else {
+            Node *temp = head;
+            while (temp->next != NULL) {
+                temp = temp->next;
+                
+            }
+        Node *curr = new Node(x);
+        temp->next = curr;
+            
+        }
+        return head;
+        
+    }
+};
+
+int main(){
+
+    Solution obj;
+    Node* head = NULL;
+    head = obj.insertAtEnd(head, 1);
+    head = obj.insertAtEnd(head, 2);
+    return 0;
+}
