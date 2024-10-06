4d4f031ef4bd4821d24f9b3a091752ceb3d9d7dc
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-10-06 10:50:58 +0530

    Create Integral_Points_Inside_Triangle.cpp

---
+++ Geeks_for_geeks/Array/easy/Integral_Points_Inside_Triangle.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+   public:
+    long long int InternalCount(long long int p[], long long int q[],long long int r[]) {
+        long long area = 0;
+        area+=(p[0]*q[1]) - (p[1]*q[0]);
+        area+=(q[0]*r[1]) - (q[1]*r[0]);
+        area+=(r[0]*p[1]) - (r[1]*p[0]);
+        long long pb = 0;
+        pb+=__gcd(abs(p[0]-q[0]),abs(p[1]-q[1]));
+        pb+=__gcd(abs(r[0]-q[0]),abs(r[1]-q[1]));
+        pb+=__gcd(abs(p[0]-r[0]),abs(p[1]-r[1]));
+        return abs(area)/2 - pb/2 +1;
+    }
+};
+
+int main(){
+    int t;
+    cin>>t;
+    long long int p[2],q[2],r[2];
+    cin>>p[0]>>p[1]>>q[0]>>q[1]>>r[0]>>r[1];
+    Solution s;
+    lond lond ans=s.InternalCount(p,q,r);
+    cout<<ans<<endl;
+    return 0;
+}
