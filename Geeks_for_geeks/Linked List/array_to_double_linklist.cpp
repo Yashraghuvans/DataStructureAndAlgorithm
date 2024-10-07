3fea2580b635318aabc6b39d8822b4fd6e705077
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-10-07 19:48:11 +0530

    Update array_to_double_linklist.cpp

--- Geeks_for_geeks/Linked List/array_to_double_linklist.cpp
+++ Geeks_for_geeks/Linked List/array_to_double_linklist.cpp
@@ -19,3 +19,29 @@ class Node{
         back = nullptr;
     }
 };
+
+Node* convert(vector<int>arr){
+    Node* head=new Node(arr[0]);
+    Node* prev=head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp=new Node(arr[i]);
+        prev->next=temp;
+        prev=temp;
+    }
+    return head;
+}
+
+void print(Node* head){
+    Node* temp=head;
+    while(temp!=nullptr){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
+int main(){
+    vector<int>arr={1,2,3,4,5};
+    Node* head=convert(arr);
+    print(head);
+    return 0;
+}
