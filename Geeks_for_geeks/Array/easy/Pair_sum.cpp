ac61792cc0e1618900bc702ac5451f402747f39d
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-10-08 22:55:19 +0530

    Create Pair_sum.cpp

---
+++ Geeks_for_geeks/Array/easy/Pair_sum.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class solution{
+  vector<vector<int>>pairSum(vector<int>&arr,int s){
+    vector<vector<int>>ans;
+    for(int i=0;i<arr.size();i++){
+      for(int j=i+1;j<arr.size();j++){
+        if(arr[i]+arr[j]==s){
+          vector<int>temp;
+          temp.push_back(min(arr[i],arr[j]));
+          temp.push_back(max(arr[i],arr[j]));
+          ans.push_back(temp);
+        }
+      }
+    }
+    sort(ans.begin(),ans.end());
+    return ans;
+  }
+};
+
+int main(){
+  vector<int>arr;
+  int sum;
+  int size;
+
+  cin>>size;
+  cin>>sum;
+
+  for(int i=0;i<size;i++){
+    cin>>arr[i];
+  }
+  
+  solution s;
+  vector<vector<int>>pair;
+  pair.push_back(s.pairSum(arr,sum));
+  return 0;
+}
