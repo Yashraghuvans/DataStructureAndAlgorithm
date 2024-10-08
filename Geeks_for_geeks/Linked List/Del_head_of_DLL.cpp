b2b79f261bed1f69a7983a7ed6ce8f59c356c427
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-10-08 22:55:03 +0530

    Create Del_head_of_DLL.cpp

---
+++ Geeks_for_geeks/Linked List/Del_head_of_DLL.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node *back;
+
+public:
+    Node(int data1, Node *next1, Node *back1)
+    {
+        data = data1;
+        next = next1;
+        back = back1;
+    }
+
+public:
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+        back = nullptr;
+    }
+};
+
+Node *convert(vector<int> arr)
+{
+    Node *head = new Node(arr[0]);
+    Node *prev = head;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i]);
+        prev->next = temp;
+        prev = temp;
+    }
+    return head;
+}
+
+Node *deleteHead(Node *head)
+{
+    if (head == NULL || head->next == NULL)
+    {
+        return NULL;
+    }
+    Node *prev = head;
+    head = head->next;
+
+    head->back = nullptr;
+    prev->next = nullptr;
+    delete prev;
+    return head;
+}
+
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != nullptr)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
