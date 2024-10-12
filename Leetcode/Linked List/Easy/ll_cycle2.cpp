cc899e6f01b17b6b413ad4aa31afe7e1f4e51131
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-10-12 22:51:39 +0530

    Update ll_cycle2.cpp

--- Leetcode/Linked List/Easy/ll_cycle2.cpp
+++ Leetcode/Linked List/Easy/ll_cycle2.cpp
@@ -36,3 +36,14 @@ public:
         return head;
     }
 };
+
+int main(){
+    Solution obj;
+    ListNode* head = new ListNode(3);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(0);
+    head->next->next->next = new ListNode(-4);
+    head->next->next->next->next = head->next;
+    obj.detectCycle(head);
+    return 0;
+}
