dac07a8b237225b8910f8825848976d2aa03ad47
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-10-14 22:47:16 +0530

    Update merge_two_sorted_ll.cpp

--- Leetcode/Linked List/Easy/merge_two_sorted_ll.cpp
+++ Leetcode/Linked List/Easy/merge_two_sorted_ll.cpp
@@ -35,5 +35,22 @@ public:
 
 int main(){
  Solution s;
+ ListNode* list1 = new ListNode(1);
+    list1->next = new ListNode(2);
+    list1->next->next = new ListNode(4);
+
+    ListNode* list2 = new ListNode(1);
+    list2->next = new ListNode(3);
+    list2->next->next = new ListNode(4);   
+
+
+ 
+    Solution solution;
+    ListNode* mergedList = solution.mergeTwoLists(list1, list2);
+    while (mergedList) {
+        cout << mergedList->val << " ";
+        mergedList = mergedList->next;
+    }
+    cout << endl;
  return 0;
 }
