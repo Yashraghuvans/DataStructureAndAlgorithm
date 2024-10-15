db8673124052a70d3c9e546a8cc97f1b1fa30834
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-10-15 09:42:42 +0530

    Create Remove_Nth_Node_from_end.cpp

---
+++ Leetcode/Linked List/Medium/Remove_Nth_Node_from_end.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ 
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* temp=head;
+        ListNode* mover=head;
+        while(n){
+            mover=mover->next;
+            n--;
+        }
+        if(mover==nullptr){
+            return head->next;
+        }
+        while(mover->next!=nullptr){
+            mover=mover->next;
+            temp=temp->next;
+
+        }
+        mover=temp->next;
+        temp->next=mover->next;
+    return head;
+
+    }
+};
