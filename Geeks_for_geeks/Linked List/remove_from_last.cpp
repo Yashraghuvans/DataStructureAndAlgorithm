f33e8e1f3d295520570f7177691fd3d3ae90f44b
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-10-18 23:02:47 +0530

    Update remove_from_last.cpp

--- Geeks_for_geeks/Linked List/remove_from_last.cpp
+++ Geeks_for_geeks/Linked List/remove_from_last.cpp
@@ -34,6 +34,25 @@ public:
 };
 
 int main(){
-      Solution s;
-      return 0;
+     
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    // Remove the 2nd node from the end   
+
+    int n = 2;
+    Solution solution;
+    ListNode* newHead = solution.removeNthFromEnd(head, n);
+
+    // Print the modified linked list
+    while (newHead != nullptr) {
+        cout << newHead->val << " ";
+        newHead = newHead->next;
+    }
+    cout << endl;
+
+    return 0;
 }
