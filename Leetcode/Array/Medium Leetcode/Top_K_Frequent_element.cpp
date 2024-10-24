60a951413e356c9f7f0c7c15b8e47177de106b0c
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-10-24 23:27:27 +0530

    Create Top_K_Frequent_element.cpp

---
+++ Leetcode/Array/Medium Leetcode/Top_K_Frequent_element.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> fmap;
+        
+        for (int n : nums) {
+            fmap[n]++;
+        }
+
+        priority_queue<pair<int, int>> priority_queue;
+        for (const auto& i : fmap) {
+            priority_queue.push({i.second, i.first});
+        }
+
+        vector<int> ans;
+        for (int i = 0; i < k; i++) {
+            ans.push_back(priority_queue.top().second);
+            priority_queue.pop();
+        }
+        return ans;
+    }
+};
