e365359fa7da201c4fda0d75048008e81712d073
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-11-23 13:29:38 +0530

    Update Two_Sum_II - Input_Array_Is_Sorted.cpp

--- Leetcode/Array/Medium Leetcode/Two_Sum_II - Input_Array_Is_Sorted.cpp
+++ Leetcode/Array/Medium Leetcode/Two_Sum_II - Input_Array_Is_Sorted.cpp
@@ -11,4 +11,18 @@ public:
         }
         return {0,0};
     }
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int left = 0, right = numbers.size() - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return {left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return{};
+    }
 };
