0e56a83f505c8fa8034abbd9ce13607dcd0d4c99
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-11-25 19:31:31 +0530

    Create test.cpp

---
+++ Geeks_for_geeks/bit manipulation/test.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    int countSetBits(int n)
+    {
+        int k = 1;
+        int ans = 0;
+        while(k<=n){
+            ans += ((n+1)/(2*k))*k;
+            ans += max(0, (n+1)%(2*k) - k);
+            k *= 2;
+        }
+        return ans;
+    }
+};
