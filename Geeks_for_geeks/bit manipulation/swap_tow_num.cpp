54402af6253fd730681fa6cf1d4171a35334736a
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-11-27 19:14:39 +0530

    Update swap_tow_num.cpp

--- Geeks_for_geeks/bit manipulation/swap_tow_num.cpp
+++ Geeks_for_geeks/bit manipulation/swap_tow_num.cpp
@@ -10,3 +10,10 @@ public:
         return {a,b};
     }
 };
+int main(){
+    Solution s;
+    int a = 10, b = 5;
+    pair<int, int> result = s.get(a, b);
+    cout << "After swapping, a = " << result.first << " and b = " << result.second << endl;
+    return 0;
+}
