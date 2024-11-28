ee4cbbbcff3c3ef7f77a402b85569df9eed9b513
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-11-29 01:31:39 +0530

    Update connected_ll.cpp

--- HackerRank/connected_ll.cpp
+++ HackerRank/connected_ll.cpp
@@ -36,6 +36,20 @@ node* print(node* head){
     cout<<"----------------------------------"<<endl;
     return head;
 }
+void connection_ll(node* head1,node* head2){
+    int value=6;
+    node* point1=head1;
+    node* point2=head2;
+    node* curr=head2;
+    while(point2->next!=nullptr){
+        point2=point2->next;
+    }
+    curr=point2;
+    for(int i=1;i<=value;i++){
+        point1=point1->next;
+    }
+    point1=curr;
+}
 int main(){
     vector<int>arr1={1,2,3,4,5,6};
     vector<int>arr2={11,22,33,44};
