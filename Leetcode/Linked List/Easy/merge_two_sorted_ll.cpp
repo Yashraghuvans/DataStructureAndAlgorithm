bf74552084c9f1a50a085c82b06c27a1731ae219
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-11-30 15:29:05 +0530

    Update merge_two_sorted_ll.cpp

--- Leetcode/Linked List/Easy/merge_two_sorted_ll.cpp
+++ Leetcode/Linked List/Easy/merge_two_sorted_ll.cpp
@@ -18,7 +18,16 @@ struct ListNode{
 };
 
 
-
+ListNode* constructor(vector<int>arr){
+    ListNode*head=new ListNode(arr[0]);
+    ListNode*mover=head;
+    for(int i=1;i<arr.size();i++){
+        ListNode*temp=new ListNode(arr[i]);
+        mover->next=temp;
+        mover=temp;
+    }
+    return head;
+}
 
 
 
