d9f583ec273828d6a795ba0551ffcabb3001f53b
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-11-30 15:28:16 +0530

    Update merge_two_sorted_ll.cpp

--- Leetcode/Linked List/Easy/merge_two_sorted_ll.cpp
+++ Leetcode/Linked List/Easy/merge_two_sorted_ll.cpp
@@ -1,56 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
- struct ListNode {
-     int val;
-     ListNode *next;
-     ListNode() : val(0), next(nullptr) {}
-     ListNode(int x) : val(x), next(nullptr) {}
-     ListNode(int x, ListNode *next) : val(x), next(next) {}
- };
+struct ListNode{
+    public:
+    int data;
+    ListNode* next;
 
-
-class Solution {
-public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* dummy = new ListNode(0);
-        ListNode* cur = dummy;
-
-        while (list1 && list2) {
-            if (list1->val > list2->val) {
-                cur->next = list2;
-                list2 = list2->next;
-            } else {
-                cur->next = list1;
-                list1 = list1->next;
-            }
-            cur = cur->next;
-        }
-
-        cur->next = list1 ? list1 : list2;
-
-        return dummy->next;        
+    public:
+    ListNode(int data1,ListNode* next1){
+        data=data1;
+        next=next1;
+    }
+    ListNode(int data1){
+        data=data1;
+        next=nullptr;
     }
 };
 
-int main(){
- Solution s;
- ListNode* list1 = new ListNode(1);
-    list1->next = new ListNode(2);
-    list1->next->next = new ListNode(4);
-
-    ListNode* list2 = new ListNode(1);
-    list2->next = new ListNode(3);
-    list2->next->next = new ListNode(4);   
 
 
- 
-    Solution solution;
-    ListNode* mergedList = solution.mergeTwoLists(list1, list2);
-    while (mergedList) {
-        cout << mergedList->val << " ";
-        mergedList = mergedList->next;
-    }
-    cout << endl;
- return 0;
-}
+
+
+
