a10581d772a9a9541dff68f7700043ec344ef0d1
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-12-01 11:24:54 +0530

    Update check_if_N_and_double_exist.cpp

--- Leetcode/Array/Easy Leetcode/check_if_N_and_double_exist.cpp
+++ Leetcode/Array/Easy Leetcode/check_if_N_and_double_exist.cpp
@@ -19,4 +19,16 @@ public:
 
         return false;
     }
+    bool checkIfExist1(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i!=j && arr[j]*2==arr[i]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 };
