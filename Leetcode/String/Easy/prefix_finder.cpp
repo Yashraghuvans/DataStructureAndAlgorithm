949d5b25848d40c45152347bb46c8b0f45407622
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-12-02 22:38:55 +0530

    Create prefix_finder.cpp

---
+++ Leetcode/String/Easy/prefix_finder.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int isPrefixOfWord(string sentence, string searchWord) {
+        istringstream stream(sentence);
+        string word;
+        int index = 1;
+        while (stream >> word) {
+            if (word.find(searchWord) == 0) {
+                return index;
+            }
+            index++;
+        }
+        return -1;
+    }
+};
