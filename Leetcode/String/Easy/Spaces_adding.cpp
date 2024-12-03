65f2e16d1783019de81dbdf5711a74a8465e30ee
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-12-03 18:50:01 +0530

    Update Spaces_adding.cpp

--- Leetcode/String/Easy/Spaces_adding.cpp
+++ Leetcode/String/Easy/Spaces_adding.cpp
@@ -1,17 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    string addSpaces(string s, vector<int>& spaces) {
-        int m=s.size();
-        int n=spaces.size();
+    string addSpaces(string s, vector<int> &spaces)
+    {
+        int m = s.size();
+        int n = spaces.size();
 
         string result;
-        int first=0;
-        int second=0;
+        int first = 0;
+        int second = 0;
 
-        while(first<m){
-            if(second<n && first==spaces[second]){
+        while (first < m)
+        {
+            if (second < n && first == spaces[second])
+            {
                 result.push_back(' ');
                 second++;
             }
@@ -20,7 +24,31 @@ public:
         }
         return result;
     }
+
+    string addSpaces1(string &s, vector<int> &spaces)
+    {
+        const int m = spaces.size(), n = s.size();
+        string t(n + m, ' ');
+        spaces.push_back(n);
+
+        int j = 0, i = 0;
+        for (i = 0; i < n; i++)
+        {
+            while (i < n && i != spaces[j])
+            {
+                t[i + j] = s[i];
+                i++;
+            }
+            if (j < m)
+            {
+                t[i + j + 1] = s[i];
+                j++;
+            }
+        }
+        return t;
+    }
 };
-int main(){
+int main()
+{
     return 0;
 }
