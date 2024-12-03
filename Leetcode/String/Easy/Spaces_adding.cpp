6ba3a2a0b516a6947a5753d14dff90e3927218d9
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-12-03 18:44:10 +0530

    Create Spaces_adding.cpp

---
+++ Leetcode/String/Easy/Spaces_adding.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string addSpaces(string s, vector<int>& spaces) {
+        int m=s.size();
+        int n=spaces.size();
+
+        string result;
+        int first=0;
+        int second=0;
+
+        while(first<m){
+            if(second<n && first==spaces[second]){
+                result.push_back(' ');
+                second++;
+            }
+            result.push_back(s[first]);
+            first++;
+        }
+        return result;
+    }
+};
