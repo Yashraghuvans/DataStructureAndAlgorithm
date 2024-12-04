87c8b811000aa29453f538b0815b8abaaa5c2f91
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-12-04 18:47:15 +0530

    Create Odd_Even_Linked_List.cpp

---
+++ Leetcode/Linked List/Medium/Odd_Even_Linked_List.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (!head || !head->next) {
+            return head;
+        }
+
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* evenHead = even;
+
+        while (odd && even && even->next) {
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
+        }
+
+        odd->next   = evenHead;
+        return head;
+    }
+};
