205ad63ca309d078c7674fd6ca7ec69d91cbfe16
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-12-05 22:07:34 +0530

    Update pair_sum.cpp

--- Leetcode/Linked List/Medium/pair_sum.cpp
+++ Leetcode/Linked List/Medium/pair_sum.cpp
@@ -1,6 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        int sz = 0;
+        ListNode* temp = head;
+        vector<int> temparr;
+        while (temp != nullptr) {
+            temparr.push_back(temp->val);
+            temp = temp->next;
+            sz++;
+        }
+        int p1 = 0, p2 = sz - 1;
+        int ans = INT_MIN;
+        while (p1 < p2) {
+            ans = max(ans, temparr[p1++] + temparr[p2--]);
+        }
+        return ans;
+    }
+};
 int main(){
     return 0;
 }
