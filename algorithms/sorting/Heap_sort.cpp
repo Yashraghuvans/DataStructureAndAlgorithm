f50e2b3d5390addaa5b85bf7da5d22e654294bf5
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-12-18 22:41:34 +0530

    Update Heap_sort.cpp

--- algorithms/sorting/Heap_sort.cpp
+++ algorithms/sorting/Heap_sort.cpp
@@ -4,7 +4,18 @@
 
 using namespace std;
 
+void heapSort(vector<int>& arr) {
+    int n = arr.size();
 
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+
+    for (int i = n - 1; i > 0; i--) {
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+}
 
 int main() {
     vector<int> arr = {12, 11, 13, 5, 6, 7};
