a4fa5e82ff8fb78c843a9a90968d634007472387
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-12-19 23:50:14 +0530

    Update Heap_sort.cpp

--- algorithms/sorting/Heap_sort.cpp
+++ algorithms/sorting/Heap_sort.cpp
@@ -1,1 +1,57 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+void heapify(vector<int>& arr, int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest]) {
+        largest = left;
+    }
+
+    if (right < n && arr[right] > arr[largest]) {
+        largest = right;
+    }
+
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+void heapSort(vector<int>& arr) {
+    int n = arr.size();
+
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+
+    for (int i = n - 1; i > 0; i--) {
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+
+int main() {
+    vector<int> arr = {12, 11, 13, 5, 6, 7};
+    int n = arr.size();
+
+    heapSort(arr);
+
+    cout << "Sorted array is \n";
+    for (int i = 0; i < n; ++i)
+        cout << arr[i] << " ";
+    cout << "\n";
+
+    vector<int> arr2 = {4, 10, 3, 5, 1, 2};
+    n = arr2.size();
+
+    heapSort(arr2);
+
+    cout << "Sorted array is \n";
+    for (int i = 0; i < n; ++i)
+        cout << arr2[i] << " ";
+    cout << "\n";
+
+    return 0;
+}
