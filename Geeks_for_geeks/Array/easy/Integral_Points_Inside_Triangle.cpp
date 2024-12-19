4c15caed07241e695d8e825241d20a8e19bef304
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-12-19 23:47:41 +0530

    update

--- Geeks_for_geeks/Array/easy/Integral_Points_Inside_Triangle.cpp
+++ Geeks_for_geeks/Array/easy/Integral_Points_Inside_Triangle.cpp
@@ -1,28 +1,37 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-class Solution {
-   public:
-    long long int InternalCount(long long int p[], long long int q[],long long int r[]) {
-        long long area = 0;
-        area+=(p[0]*q[1]) - (p[1]*q[0]);
-        area+=(q[0]*r[1]) - (q[1]*r[0]);
-        area+=(r[0]*p[1]) - (r[1]*p[0]);
-        long long pb = 0;
-        pb+=__gcd(abs(p[0]-q[0]),abs(p[1]-q[1]));
-        pb+=__gcd(abs(r[0]-q[0]),abs(r[1]-q[1]));
-        pb+=__gcd(abs(p[0]-r[0]),abs(p[1]-r[1]));
-        return abs(area)/2 - pb/2 +1;
+#include <bits/stdc++.h> 
+vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m)
+{
+	vector<int>ans;
+	map<int,int>mpp;
+	for(int i=0;i<n;i++){
+		mpp[arr1[i]]++;
+	}
+	for(int i=0;i<m;i++){
+		 if (mpp.count(arr2[i])) {
+            ans.push_back(arr2[i]);
+            mpp[arr2[i]]--; 
+            if (mpp[arr2[i]] == 0) {
+                mpp.erase(arr2[i]); 
+            }
+        }
+	}
+	return ans;
+}
+vector<int> findArrayIntersection1(vector<int> &arr1, int n, vector<int> &arr2, int m)
+{
+	vector<int> ans;
+    int i = 0, j = 0;
+    while (i < n && j < m) {
+        if (arr1[i] == arr2[j]) {
+            ans.push_back(arr1[i]);
+            i++;
+            j++;
+        } else if (arr1[i] < arr2[j]) {
+            i++;
+        } else {
+            j++;
+        }
     }
-};
 
-int main(){
-    int t;
-    cin>>t;
-    long long int p[2],q[2],r[2];
-    cin>>p[0]>>p[1]>>q[0]>>q[1]>>r[0]>>r[1];
-    Solution s;
-    lond lond ans=s.InternalCount(p,q,r);
-    cout<<ans<<endl;
-    return 0;
+    return ans;
 }
