bfff2a69d2cac3902408114a905c87cd46890fb3
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-12-20 22:17:03 +0530

    Update radix_sort.cpp

--- algorithms/sorting/radix_sort.cpp
+++ algorithms/sorting/radix_sort.cpp
@@ -2,6 +2,15 @@
 #include <vector>
 
 using namespace std;
+int getMax(vector<int>& arr, int n) {
+    int mx = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > mx)
+            mx = arr[i];
+    return mx;
+}
+
+
 
 int main() {
     vector<int> arr = {170, 45, 75, 90, 802, 24, 2, 66};
