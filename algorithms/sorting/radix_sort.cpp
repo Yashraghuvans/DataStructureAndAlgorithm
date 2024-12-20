ec2aa72607a92c3dd439aafc2d4e58dd0ec1e8f6
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-12-20 22:17:27 +0530

    Update radix_sort.cpp

--- algorithms/sorting/radix_sort.cpp
+++ algorithms/sorting/radix_sort.cpp
@@ -9,8 +9,12 @@ int getMax(vector<int>& arr, int n) {
             mx = arr[i];
     return mx;
 }
+void radixsort(vector<int>& arr, int n) {
+    int m = getMax(arr, n);
 
-
+    for (int exp = 1; m / exp > 0; exp *= 10)
+        countSort(arr, n, exp);
+}
 
 int main() {
     vector<int> arr = {170, 45, 75, 90, 802, 24, 2, 66};
