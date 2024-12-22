2cae6d0c3eb5e34c86c0e88bc4c621703831f897
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-12-22 21:47:48 +0530

    Update Adding_at_end.cpp

--- Geeks_for_geeks/Linked List/Adding_at_end.cpp
+++ Geeks_for_geeks/Linked List/Adding_at_end.cpp
@@ -2,20 +2,6 @@
 #include <iostream>
 using namespace std;
 
-class Node{
-    public:
-    int data;
-    Node* next;
-
-    Node(int data1,Node* next1){
-        data = data1;
-        next = next1;
-    }
-    Node(int data1){
-        data = data1;
-        next = nullptr;
-    }
-};
 
 class Solution {
   public:
