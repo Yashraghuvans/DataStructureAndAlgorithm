5e8d0de7af55ddf23c6ddfa28237e61ee6e5d69c
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-01-03 20:45:22 +0530

    Update Find_position.cpp

--- Leetcode/Binary Search/Easy/Find_position.cpp
+++ Leetcode/Binary Search/Easy/Find_position.cpp
@@ -3,26 +3,24 @@ using namespace std;
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int low=0;
-        int high=nums.size();
+        int low = 0;
+        int high = nums.size();
         int mid;
-        if(target>nums[high-1]){
+        if (target > nums[high - 1]) {
             return high;
         }
-        while(low<=high){
-              mid=(low+high)/2;
-            if(nums[mid]==target){  
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (nums[mid] == target) {
                 return mid;
             }
-          
-            if(target<nums[mid]){     
-            high=mid-1;    
-            }else{
-            low=mid+1;        
+            if (target < nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
-          
         }
-         return  low;   
+        return low;
     }
 };
 
