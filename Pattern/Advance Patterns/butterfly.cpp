8e5842c64f3e919045d13e52c5a0442a5357b808
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-01-17 22:42:00 +0530

    Update butterfly.cpp

--- Pattern/Advance Patterns/butterfly.cpp
+++ Pattern/Advance Patterns/butterfly.cpp
@@ -4,20 +4,50 @@ using namespace std;
 class Patterns
 {
 public:
-    void butterfly(int x)
+    void butterfly(int n)
     {
+        int x=2*n-1;
+        for(int i=0;i<x;i++){
+            for(int j=0;j<i;j++){
+                cout<<"*";
+            }
+            for(int j=x;j>i;j--){
+                cout<<" ";
+            }
+            for(int j=x;j>i;j--){
+                cout<<" ";
+            }
+            for(int j=0;j<i;j++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+        for(int i=x;i>0;i--){
+            for(int j=0;j<i;j++){
+                cout<<"*";
+            }
+            for(int j=x;j>i;j--){
+                cout<<" ";
+            }
+            for(int j=x;j>i;j--){
+                cout<<" ";
+            }
+            for(int j=0;j<i;j++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
     }
 };
 int main()
 {
 
     // calling value of X
-    int x;
-    cout << "Enter the value of X : ";
-    cin >> x;
+    int x=5;
+    
     Patterns p;
 
     p.butterfly(x);
-    
+
     return 0;
 }
