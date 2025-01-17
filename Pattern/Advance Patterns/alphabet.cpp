8d50c8ed8e9c6193dd46fb0713f48404ab859d93
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-01-17 23:14:24 +0530

    Update alphabet.cpp

--- Pattern/Advance Patterns/alphabet.cpp
+++ Pattern/Advance Patterns/alphabet.cpp
@@ -1,14 +1,39 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Pattern{
-    public:
-    void alphabet(int n){
-
+class Pattern
+{
+public:
+    void alphabet(int n)
+    {
+        int x = n;
+        char start = 'A'; 
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                cout << (char)(start + j); 
+            }
+            for (int j = x; j > i; j--)
+            {
+                cout << " ";
+            }
+            for (int j = x; j > i; j--)
+            {
+                cout << " ";
+            }
+            for (int j = 0; j < i; j++)
+            {
+                cout << (char)(start + j); 
+            }
+            cout << endl;
+        }
     }
 };
-int main(){
-    int x=10;
+
+int main()
+{
+    int x = 10;
     Pattern p;
     p.alphabet(x);
     return 0;
