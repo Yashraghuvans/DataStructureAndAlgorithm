1491558dcf49b6ed4e316a01b1dfc08bf923678b
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-01-17 23:19:48 +0530

    Create hourglass.cpp

---
+++ Pattern/Advance Patterns/hourglass.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+class Pattern{
+    public:
+    void hourglass(int n){
+        
+    }
+};
+int main(){
+    int x=10;
+    Pattern p;
+    p.hourglass(x);
+    return 0;
+}
