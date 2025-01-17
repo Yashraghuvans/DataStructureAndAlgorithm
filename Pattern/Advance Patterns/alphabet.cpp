1b9291890f96ea2f85ac8215cc6ed57979cf905d
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-01-17 22:56:10 +0530

    Create alphabet.cpp

---
+++ Pattern/Advance Patterns/alphabet.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+class Pattern{
+    public:
+    void alphabet(int n){
+
+    }
+};
+int main(){
+    int x=10;
+    Pattern p;
+    p.alphabet(x);
+    return 0;
+}
