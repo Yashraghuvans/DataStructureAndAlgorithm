a4bcd2b813793ac90976d64cbe7c5d0cd1b5e69e
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-01-17 22:07:21 +0530

    Create butterfly.cpp

---
+++ Pattern/Advance Patterns/butterfly.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Patterns
+{
+public:
+    void butterfly(int x)
+    {
+    }
+};
+int main()
+{
+
+    // calling value of X
+    int x;
+    cout << "Enter the value of X : ";
+    cin >> x;
+    Patterns p;
+
+    p.butterfly(x);
+    
+    return 0;
+}
