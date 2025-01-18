f21f51d764900b7aacc43bb4ef093484d01f2f74
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-01-18 20:01:05 +0530

    Create hollow_square.cpp

---
+++ Pattern/Advance Patterns/hollow_square.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+class Pattern{
+    public:
+    void hollow_square(int n){
+
+    }
+};
+int main(){
+    int x;
+    Pattern p;
+    p.hollow_square(x);
+    return 0;
+}
