e435abc0489c65e292a6b4b057e6d5e49f2aac79
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-01-19 23:38:14 +0530

    Update hollow_square.cpp

--- Pattern/Advance Patterns/hollow_square.cpp
+++ Pattern/Advance Patterns/hollow_square.cpp
@@ -4,28 +4,28 @@ using namespace std;
 class Pattern
 {
 public:
-    void hollow_square(int n ,int m)
+    void hollow_square(int n, int m)
     {
-        int i, j; 
-    for (i = 1; i <= n; i++) 
-    { 
-        for (j = 1; j <= m; j++) 
-        { 
-            if (i == 1 || i == n || 
-                j == 1 || j == m)         
-                cout << "*";             
-            else
-                cout << " ";
-        } 
-        cout << endl;
-    } 
+        int i, j;
+        for (i = 1; i <= n; i++)
+        {
+            for (j = 1; j <= m; j++)
+            {
+                if (i == 1 || i == n ||
+                    j == 1 || j == m)
+                    cout << "*";
+                else
+                    cout << " ";
+            }
+            cout << endl;
+        }
     }
 };
 int main()
 {
     int x = 10;
-    int y=8;
+    int y = 8;
     Pattern p;
-    p.hollow_square(x,y);
+    p.hollow_square(x, y);
     return 0;
 }
