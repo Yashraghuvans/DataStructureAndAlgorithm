6024fcc8c067dc86860b23ce5bcae307a7670990
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-01-19 23:13:23 +0530

    Update hollow_square.cpp

--- Pattern/Advance Patterns/hollow_square.cpp
+++ Pattern/Advance Patterns/hollow_square.cpp
@@ -4,11 +4,16 @@ using namespace std;
 class Pattern{
     public:
     void hollow_square(int n){
-
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
     }
 };
 int main(){
-    int x;
+    int x=10;
     Pattern p;
     p.hollow_square(x);
     return 0;
