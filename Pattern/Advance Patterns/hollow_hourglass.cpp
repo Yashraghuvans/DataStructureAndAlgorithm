a452704ca7795b296f5ab8fa11d4c23bc6c13e26
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-01-20 21:01:08 +0530

    Update hollow_hourglass.cpp

--- Pattern/Advance Patterns/hollow_hourglass.cpp
+++ Pattern/Advance Patterns/hollow_hourglass.cpp
@@ -3,7 +3,30 @@ using namespace std;
 
 class Pattern {
 public:
+        void hourglass(int n) {
+        int x = 2 * n - 1;
+        char start = 'A'; 
 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                cout << " ";
+            }
+            for (int j = 0; j < x - 2 * i; j++) {
+                cout << (char)(start + j);
+            }
+            cout << endl;
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                cout << " ";
+            }
+            for (int j = 0; j < x - 2 * i; j++) {
+                cout << (char)(start + j);
+            }
+            cout << endl;
+        }
+    }
 };
 
 int main() {
