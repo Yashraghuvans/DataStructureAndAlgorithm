a7edf80eeb52b3cb67aab3647d297223236e58bb
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-01-20 21:09:36 +0530

    Update hollow_hourglass.cpp

--- Pattern/Advance Patterns/hollow_hourglass.cpp
+++ Pattern/Advance Patterns/hollow_hourglass.cpp
@@ -1,36 +1,70 @@
 #include <iostream>
 using namespace std;
 
-class Pattern {
+class Pattern
+{
 public:
-        void hourglass(int n) {
-        int x = 2 * n - 1;
-        char start = 'A'; 
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
+    void hourglass(int n)
+    {
+        
+        for (int i = n; i >= 1; i--)
+        {
+            
+            for (int j = 1; j <= n - i; j++)
+            {
                 cout << " ";
             }
-            for (int j = 0; j < x - 2 * i; j++) {
-                cout << (char)(start + j);
+
+           
+            cout << char('A' + i - 1);
+
+            
+            for (int j = 2; j <= 2 * (i - 1) - 1; j++)
+            {
+                cout << " ";
             }
+
+           
+            if (i > 1)
+            {
+                cout << char('A' + i - 1);
+            }
+
             cout << endl;
         }
 
-        for (int i = n - 2; i >= 0; i--) {
-            for (int j = 0; j < i; j++) {
+        
+        for (int i = 2; i <= n; i++)
+        {
+            
+            for (int j = 1; j <= n - i; j++)
+            {
                 cout << " ";
             }
-            for (int j = 0; j < x - 2 * i; j++) {
-                cout << (char)(start + j);
+
+            
+            cout << char('A' + i - 1);
+
+            
+            for (int j = 2; j <= 2 * (i - 1) - 1; j++)
+            {
+                cout << " ";
             }
+
+            
+            if (i > 1)
+            {
+                cout << char('A' + i - 1);
+            }
+
             cout << endl;
         }
     }
 };
 
-int main() {
-    int x = 5;
+int main()
+{
+    int x = 15;
     Pattern p;
     p.hourglass(x);
     return 0;
