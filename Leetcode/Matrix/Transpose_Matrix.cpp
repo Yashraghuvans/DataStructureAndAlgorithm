1f74b3d844289435d17c9b45a8122c9d17f7cc77
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-01-27 03:06:11 +0530

    Create Transpose_Matrix.cpp

---
+++ Leetcode/Matrix/Transpose_Matrix.cpp
@@ -0,0 +1,20 @@
+#include<bitset>/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        vector<vector<int>>ans(n,vector<int>(m));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                ans[j][i]=matrix[i][j];
+            }
+        }
+        return ans;
+    }
+};
+int main(){
+    Solution s;
+    return 0;
+}
